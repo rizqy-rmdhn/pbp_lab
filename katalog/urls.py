864a2af69e596c3dd6606e6792e162101f8ab39a
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-09-12 16:09:48 +0700

    Feature(Catalog): Implement views

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from .views import show_catalog
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+                'list_barang': data_barang_catalog,
+                'name': 'Muhammad Rizqy Ramadhan',
+                'student_id' : '2106632182',
+              }
+    return render(request, "katalog.html", context)
