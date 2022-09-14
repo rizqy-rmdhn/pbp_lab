65daf97224676ee2ec6e644ca8edfdc0dc867129
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-09-14 22:15:20 +0700

    Implement tests.py

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client
+
+
 
 # Create your tests here.
+class KatalogTest(TestCase):
+    def test_katalog_url_exists(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_katalog_template(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
